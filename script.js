977a5b4e63efd2b7e1007269fac83ff846078770
Mirhan <111046193+NathanielConstructive@users.noreply.github.com>
2024-02-28 08:29:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+// Declare initial variables
+let player = "X"
+let winner = ""
+let play_board = ["", "", "", "", "", "", "", "", ""]
+
+// Function to check for a winner
+const checkWinner = () => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  for (let line of lines) {
+    const [a, b, c] = line;
+    if (
+      play_board[a] &&
+      play_board[a] === play_board[b] &&
+      play_board[a] === play_board[c]
+    ) {
+      winner = play_board[a]; // Set the winner
+      return true; // Return true if there's a winner
+    }
+  }
+  return false; // Return false if no winner
+};
+
+// Function to reset the board
+const reset_board = () => {
+  play_board = ["", "", "", "", "", "", "", "", ""] // Reset the board array
+  // Reset the visual board
+  document.querySelectorAll(".block").forEach((block) => {
+    block.textContent = ""
+    block.classList.remove("occupied")
+  })
+  document.getElementById("winner_message").textContent = ""
+};
+
+const addPlayerMove = (index) => {
+  if (play_board[index] === "") {
+    play_board[index] = player;
+    document.getElementById(`block_${index}`).textContent = player;
+    document.getElementById(`block_${index}`).classList.add("occupied");
+  } else {
+    alert(`Block ${index + 1} is already occupied!`)
+    return;
+  }
+
+  if (checkWinner()) {
+    document.getElementById("winner_message").textContent = `Player ${player} wins!`
+  } else {
+    player = player === "X" ? "O" : "X"
+  }
+}
+
+const cells = document.querySelectorAll('.block')
+let currentPlayer = 'X'
+
+cells.forEach(cell => {
+  cell.addEventListener('click', () => {
+    const index = parseInt(cell.id.split('_')[1]);
+    if (!cell.textContent) {
+      addPlayerMove(index);
+      if (checkWinner()) {
+        alert(`Player ${currentPlayer} wins!`)
+        reset_board();
+      } else if (isDraw()) {
+        alert('It\'s a draw!')
+        reset_board();
+      } else {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
+      }
+      cell.textContent = currentPlayer;
+    }
+  })
+})
+
+const isDraw = () => {
+  return play_board.every(cell => cell !== '') && !checkWinner();
+};   
